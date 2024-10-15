8def279f03a743188b7ba8e19e0bdba84958d35e
Pratham Chauhan (PC) <prathamchauhan.coding42@gmail.com>
2024-10-16 01:40:26 +0530

    get geolocation

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
+
 // Call the IP geolocation API
-function fetch(url) {
+
+var user_country;
+var user_geoinfo;
+function get_location() {
   fetch("https://ipinfo.io/json")
     .then((response) => response.json())
     .then((data) => {
-      // Extract and display the country
       console.log("Country:", data);
-      document.getElementById(
-        "country"
-      ).textContent = `You are visiting from ${data.city}, ${data.country}`;
+      user_geoinfo = data;
     })
     .catch((error) => {
       console.error("Error fetching location:", error);
-      document.getElementById("country").textContent =
-        "Could not determine your country.";
     });
 }
-
+get_location();
 
 // change opacity of arrow id element to 1 after 10 sec
 setTimeout(() => document.getElementById("svg-container").style.opacity = "1", 8000)
